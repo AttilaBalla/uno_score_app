58dd442601804a7465d526b7f4e0c750f55a3cd8
AttilaBalla <xattus@gmail.com>
2020-08-07 22:19:21 +0200

    Updates

---
+++ precache-manifest.9d8f24f24fec90466460e6f88db4b043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53fc40c727bad0bef29df1b4d8966cb",
+    "url": "/uno_score_app/index.html"
+  },
+  {
+    "revision": "43e6a22b083de77dfaa1",
+    "url": "/uno_score_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a781257b45f710178a10",
+    "url": "/uno_score_app/static/js/2.c6c36d96.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/uno_score_app/static/js/2.c6c36d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e6a22b083de77dfaa1",
+    "url": "/uno_score_app/static/js/main.93a1346b.chunk.js"
+  },
+  {
+    "revision": "2f4ed85eac7d0815b0ac",
+    "url": "/uno_score_app/static/js/runtime-main.3d49f045.js"
+  }
+]);
